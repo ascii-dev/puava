73b5b7bc8d6ae5d8ee8d1f6ca824fc278c2a9ffa
Samuel Raphael <samueloraphael@gmail.com>
2025-11-24 19:05:31 +0100

    setup and ensure that c repo structure works with bazel

---
+++ c/includes/greet.h
@@ -0,0 +1,8 @@
+#ifndef PUAVA_GREET_H
+#define PUAVA_GREET_H
+
+// A simple function to test our build system.
+// Returns 1 (success)
+int puava_greet(void);
+
+#endif // PUAVA_GREET_H
---
+++ c/src/greet/greet.c
@@ -0,0 +1,7 @@
+#include "includes/greet.h"
+#include <stdio.h>
+
+int puava_greet(void) {
+    printf("Hello from Puava C Library!\n");
+    return 1;
+}
